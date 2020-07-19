d57fea12c2c2db5fc28e0a92d53e079bca9bbb55
cigar666 <37494715+cigar666@users.noreply.github.com>
2020-07-19 16:47:39 +0800

    Create my_imports.py

---
+++ my_imports.py
@@ -0,0 +1,5 @@
+from my_manim_projects.my_utils.my_text import *
+from my_manim_projects.my_utils.anim_effects import *
+from my_manim_projects.my_utils.my_geometry import *
+from my_manim_projects.my_utils.my_3D_mobject import *
+
